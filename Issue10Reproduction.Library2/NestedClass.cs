4e8ed47c24d6c712af6ecd375eefc8c79e54d0ff
Aragas <personal@aragas.org>
2022-04-17 17:41:47 +0300

    Init

---
+++ Issue10Reproduction.Library2/NestedClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Issue10Reproduction.Library2
+{
+    public enum NestedClassDiscriminator
+    {
+        Test1,
+        Test2,
+    }
+    public abstract partial record NestedClass([JsonDiscriminator] NestedClassDiscriminator EventType);
+    public record Test1NestedClass(string Arg1) : NestedClass(NestedClassDiscriminator.Test1);
+    public record Test2NestedClass(string Arg1) : NestedClass(NestedClassDiscriminator.Test2);
+}
